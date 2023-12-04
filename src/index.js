01e0ff4ed33ff421ba3c16ca7032dda05e12cb80
José Espinoza <jose.espinoza.3d@gmail.com>
2023-12-04 07:55:38 -0500

    feat: install and configure webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { initialCards } from "./modules/constants.js";
+import { initialCards } from "../scripts/modules/constants.js";
 import {
   setPageButtonHandler,
   renderCards,
   handleEditProfileButtonClick,
   handleAddCardButtonClick,
-} from "./modules/utils.js";
-import { globalConfig } from "./modules/config.js";
+} from "../scripts/modules/utils.js";
+import { globalConfig } from "../scripts/modules/config.js";
 
 setPageButtonHandler(
   globalConfig.editProfileButtonSelector,
