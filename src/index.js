cfd523e272c262030d57a239d5e654619c3eb249
José Espinoza <jose.espinoza.3d@gmail.com>
2023-12-05 23:19:22 -0500

    refactor: move code to src folder for webpack building

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { initialCards } from "../scripts/modules/constants.js";
+import { initialCards } from "./scripts/modules/constants.js";
 import {
   setPageButtonHandler,
   renderCards,
   handleEditProfileButtonClick,
   handleAddCardButtonClick,
-} from "../scripts/modules/utils.js";
-import { globalConfig } from "../scripts/modules/config.js";
+} from "./scripts/modules/utils.js";
+import { globalConfig } from "./scripts/modules/config.js";
 
 setPageButtonHandler(
   globalConfig.editProfileButtonSelector,
