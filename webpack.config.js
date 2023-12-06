6130c16ca2eee971acc449475572f93e6717ff33
José Espinoza <jose.espinoza.3d@gmail.com>
2023-12-05 23:43:37 -0500

    feat: configure webpack to work with html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   devtool: "inline-source-map",
@@ -6,10 +7,12 @@ module.exports = {
     main: "./src/index.js",
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     publicPath: "",
+    clean: true,
   },
+  target: ["web", "es5"],
   mode: "development",
   devServer: {
     static: path.resolve(__dirname, "./dist"),
@@ -17,4 +20,18 @@ module.exports = {
     compress: true,
     open: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
